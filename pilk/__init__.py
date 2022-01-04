4a5e84db5485a5c298d1d92cc07da41b8c370392
foyou <yimi.0822@qq.com>
2022-01-04 12:22:44 +0800

    add get_duration function

--- pilk/__init__.py
+++ pilk/__init__.py
@@ -1,1 +1,25 @@
 from ._pilk import *
+
+
+def get_duration(silk_path: str, frame_ms: int = 20) -> int:
+    """获取 silk 文件持续时间，单位：ms"""
+    with open(silk_path, 'rb') as silk:
+        tencent = False
+        if silk.read(1) == b'\x02':
+            tencent = True
+        silk.seek(0)
+        if tencent:
+            silk.seek(10)
+        else:
+            silk.seek(9)
+        i = 0
+        while True:
+            size = silk.read(2)
+            if len(size) != 2:
+                break
+            size = size[0] + size[1] << 8
+            if not tencent and size == 0xffff:
+                break
+            i += 1
+            silk.seek(silk.tell() + size)
+        return i * frame_ms
