26e25e20a007a336f17755d99ba1fa91c33b35c1
foyou <yimi.0822@qq.com>
2022-01-04 13:24:13 +0800

    add metadata

--- pilk/__init__.py
+++ pilk/__init__.py
@@ -1,5 +1,15 @@
 from ._pilk import *
 
+__title__ = 'pilk'
+__description__ = 'python silk codec binding'
+__url__ = 'https://github.com/foyoux/pilk'
+__version__ = '0.0.1'
+__author__ = 'foyou'
+__author_email__ = 'yimi.0822@qq.com'
+__license__ = 'Apache 2.0'
+__copyright__ = f'Copyright 2022 {__author__}'
+__ide__ = 'PyCharm - https://www.jetbrains.com/pycharm/'
+
 
 def get_duration(silk_path: str, frame_ms: int = 20) -> int:
     """获取 silk 文件持续时间，单位：ms"""
