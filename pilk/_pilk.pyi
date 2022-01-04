6e93ccebe5ad14d249fe04003682a5686a272e2c
foyou <yimi.0822@qq.com>
2022-01-04 16:44:44 +0800

    update .pyi file

--- pilk/_pilk.pyi
+++ pilk/_pilk.pyi
@@ -1,6 +1,6 @@
 def encode(
         pcm: str,
-        silk: str = None,
+        silk: str,
         pcm_rate: int = 24000,
         silk_rate: int = None,
         tencent: bool = False,
@@ -13,8 +13,8 @@ def encode(
 ) -> tuple[str, int]:
     """silk 编码
 
-    :param pcm: str: pcm 格式文件路径
-    :param silk: str[可选]: 编码完成的 silk 文件路径, 默认为 `pcm`参数 + '.silk'
+    :param pcm: str: pcm 输入文件路径
+    :param silk: str: 输出保存 silk 的文件路径
     :param pcm_rate: int(hz)[可选]: 输入 pcm 文件的 rate, 默认: 24000. 可选值为 [8000, 12000, 16000, 24000, 32000, 44100, 48000]
     :param silk_rate: int(8000~48000 hz)[可选]: 默认: 与 pcm_rate 一致,输出 silk 文件的 rate
     :param tencent: bool[可选]: 默认: False, 是否兼容腾讯系语音
@@ -30,14 +30,14 @@ def encode(
 
 def decode(
         silk: str,
-        pcm: str = None,
+        pcm: str,
         pcm_rate: int = 24000,
         packet_loss: int = 0
 ) -> tuple[str, int]:
     """silk 解码
 
     :param silk: str： silk 输入文件路径
-    :param pcm: str[可选]： pcm 输出文件路径, 未提供则使用 `silk` 参数 + '.pcm'
+    :param pcm: str： 输出保存 pcm 的文件路径
     :param pcm_rate: int(hz)[可选]: 输入 pcm 文件的 rate, 默认: 24000. 可选值为 [8000, 12000, 16000, 24000, 32000, 44100, 48000]
     :param packet_loss: Uplink loss estimate, in percent (0-100); default: 0
     :return: (<pcm 文件路径>, <pcm 文件时间>)
