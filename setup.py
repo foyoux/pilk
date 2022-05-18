cb9367b948a003155ee062556044e7a7368af0c9
foyou <yimi.0822@qq.com>
2022-05-18 14:21:18 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md', encoding='utf8') as f:
 # noinspection SpellCheckingInspection
 setup(
     name='pilk',
-    version='0.0.2',
+    version='0.1.0',
     description='python silk voice library',
     long_description=long_description,
     long_description_content_type='text/markdown',
